caca2450cdeaa610ec1d64549cf442ac12de1261
Roberto Mazzarello <rmazzarello@gmail.com>
2024-07-11 17:02:08 -0300

    Agregamos los repositorios que teniamos en github al portfolio

--- src/components/CardWorks.vue
+++ src/components/CardWorks.vue
@@ -2,11 +2,11 @@
     <div
         class="rounded bg-white shadow flex"
     >
-        <a :href="work.urlWeb" target="_blank" rel="noopener noreferrer">
+        <a :href="work.urlWeb" target="_blank" rel="noopener noreferrer" class="flex-shrink-0 w-48 h-48">
             <img 
                 :src="`/img/${work.imageSrc}.jpg`"
                 :alt="work.imageSrc"
-                class="p-5 min-w-96"
+                class="object-cover w-full h-full p-3"
             >
         </a>
         <div class="flex flex-col justify-between flex-grow">
@@ -16,6 +16,7 @@
             </div>
             <div class="pr-5 pb-5 self-end">
                 <p class="text-gray-500  text-sm font-bold text-right pt-2 space-x-9">
+                    <a :href="work.urlWeb" target="_blank" rel="noopener noreferrer" class="hover:text-gray-800">Visitar Web</a>
                     <a :href="work.urlGithub" target="_blank" rel="noopener noreferrer" class="hover:text-gray-800">Ver repositorio en GitHub</a>
                     <a v-if="work.urlGithub2" :href="work.urlGithub2" target="_blank" rel="noopener noreferrer" class="hover:text-gray-800">Ver repositorio en Github API</a>
                 </p>
--- src/views/MyWorkView.vue
+++ src/views/MyWorkView.vue
@@ -22,31 +22,73 @@
             urlWeb : 'https://front-barber-shop.vercel.app/auth/login',
             urlGithub : 'https://github.com/RobertoMaz/FrontBarberShop',
             urlGithub2 : 'https://github.com/RobertoMaz/ApiBarberShop',
-            imageSrc: 'barberiaapp',
-            title: 'App para barberia',
+            imageSrc: 'barber',
+            title: 'App Turnos para barberia ',
             paragraph: 'Esta App tiene una conexion a una ApiRest la cual te permite manejar un sistema de logueo, recupero de password y confirmacion de email. Ademas de poder almacenar mediante un CRUD las citas que el usuario realice.   Maneja un sistema de usuario comun y usuario ADMIN, tiene vistas para el ADMIN donde podra ver las citas del dia y posteriores en orden. Para el usuario vistas donde puede elegir el tipo de servicio, dia y hora del turno.'
             },
             {
             urlWeb : 'https://point-of-sale-weld.vercel.app/',
             urlGithub : 'https://github.com/RobertoMaz/point-of-sale',
             imageSrc: 'point-of-sale',
-            title: 'App tienda de indumentaria',
+            title: 'App Tienda de indumentaria',
             paragraph: 'Esta es una App que te permite administrar una tienda. Esta conectado con Firebase lo cual nos permite hacer un CRUD para los productos, ademas de tener un modulo de ventas donde tenemos un calendario para ver el historial de las ventas y un listado con el total del dia seleccionado. Tambien tiene la opcion de canjear cupones de descuento.'
             },
             {
             urlWeb : 'https://venta-propiedades.vercel.app/',
             urlGithub : 'https://github.com/RobertoMaz/vue-inmobiliaria',
             imageSrc: 'propiedades',
-            title: 'App venta de propiedades',
+            title: 'App Venta de propiedades',
             paragraph: 'Esta es una App para administrar venta de propiedades. Usa Firebase, para almacenar las imagenes y los datos de las propiedades. Tiene un sistema de logueo para un administrador, el cual puede hacer un CRUD con las propiedades.'
             },
             {
             urlWeb : 'https://cocktails-search.vercel.app/',
             urlGithub : 'https://github.com/RobertoMaz/cocktails-vue',
             imageSrc: 'cocktail',
-            title: 'App para buscar receta de coktails',
-            paragraph: 'Esta es una App que usa la coneccion a una Api, y nos muestra recetas para preparar cockteles, estan acomodados por categoria. Usa local storage para almacenar la informacion y no perder lo que el usuario estaba buscando o alla guardado en el apartado de favoritos.'
+            title: 'App Buscador de recetas para cócteles',
+            paragraph: 'Esta es una App que usa la coneccion a una Api, y nos muestra recetas para preparar cócteles, estan acomodados por categoria. Usa local storage para almacenar la informacion y no perder lo que el usuario estaba buscando o alla guardado en el apartado de favoritos.'
             },
+            {
+            urlWeb : 'https://customer-client-brown.vercel.app/',
+            urlGithub : 'https://github.com/RobertoMaz/customer-client',
+            imageSrc: 'adminclients',
+            title: 'App Administrador de clientes',
+            paragraph: 'Esta es una App que nos permite administrar una cartelera de clientes, en ella podemos agregar, editar y eliminar clientes que tengamos. Ademas de tener una opciona para marcar si siguen activos o no.'
+            },
+            {
+            urlWeb : 'https://weather-finder-eight-chi.vercel.app/',
+            urlGithub : 'https://github.com/RobertoMaz/weather-finder',
+            imageSrc: 'weather',
+            title: 'App Buscador del clima',
+            paragraph: 'Esta App consulta una API, que nos trae el estado del clima y algunos paises para selecciona. Ademas de tener un input dondne podemos colocar una ciudad y si esa ciudad esta en la respuesta de la API nos muestra el clima de dicha ciudad.'
+            },
+            {
+            urlWeb : 'https://cripto-search.vercel.app/',
+            urlGithub : 'https://github.com/RobertoMaz/cripto-vue',
+            imageSrc: 'cripto',
+            title: 'App Cotizador de criptomoneda',
+            paragraph: 'Esta App consulta una API ,y nos devuelve la cotizacion del dia de distintos tipos de monedas y distintos tipos de criptomoneda.'
+            },
+            {
+            urlWeb : 'https://expense-planner-eta.vercel.app/',
+            urlGithub : 'https://github.com/RobertoMaz/expense-planner',
+            imageSrc: 'expense',
+            title: 'App Planificador de gastos',
+            paragraph: 'Con esta App, podemos planificar los gastos del mes, o del dia. Nos permite agregar un presupuesto y despues podemos ir agregando los gastos que vamos teniendo. Tenemos distintas categorias y a medida que vamos agregando gastos, nos va mostranndo la cantidad disponible que tenemos de nuestro presupuesto. '
+            },
+            {
+            urlWeb : 'https://vet-note.vercel.app/',
+            urlGithub : 'https://github.com/RobertoMaz/vet-note',
+            imageSrc: 'vet',
+            title: 'App Administrador Pacientes Veterinaria',
+            paragraph: 'Con esta App, podemos administrar y llevar un registro de los pacientes que tenemos en una veterinaria por ejemplo. Podemos agregar pacientes nuevos, editar sus datos y eliminar al paciente.'
+            },
+            {
+            urlWeb : 'https://vet-note.vercel.app/',
+            urlGithub : 'https://github.com/RobertoMaz/guitar-market',
+            imageSrc: 'guitar',
+            title: 'App Tienda de guitarras',
+            paragraph: 'Esta es una tienda de guitarras, donde tenemos varios modelos y un carrit al cual podemos ir agregandole guitarras. Si seleccionamos la misma guitarra 2 veces, se suma una unidad mas al carrito, con un limite de hasta 5 por producto.'
+            }
         ]
     })
 
